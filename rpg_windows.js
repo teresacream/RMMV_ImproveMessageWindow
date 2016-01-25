56707e71d58fc0be9de1a8cba3ed049cb8ef4f0b
teresacream <teresacream@gmail.com>
2016-01-25 18:03:44 +0800

    Update: 對不起我也忘了我改了什麼

--- rpg_windows.js
+++ rpg_windows.js
@@ -4270,7 +4270,7 @@ Window_Message.prototype.initialize = function() {
     var width = this.windowWidth();
     var height = this.windowHeight();
     var x = (Graphics.boxWidth - width) / 2;
-    Window_Base.prototype.initialize.call(this, 100, 0, width, height);
+    Window_Base.prototype.initialize.call(this, x, 0, width, height);
     this.openness = 0;
     this.initMembers();
     this.createSubWindows();
