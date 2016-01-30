7a7c24aacb7eea065bd2fdb3c276620bb3356cfe
teresacream <teresacream@users.noreply.github.com>
2016-01-30 18:46:47 +0800

    Update getWindowPosition.js
    : 初始化flag

--- plugins/getWindowPosition.js
+++ plugins/getWindowPosition.js
@@ -7,15 +7,13 @@ function getWindowPosition()
 	throw new Error('This is a static class');
 }
 
-/*getWindowPosition.x = 0;		//eventX
-getWindowPosition.y = 0;		//eventY
 getWindowPosition.flag = 0;	//if 1, move the window above event*/
 
 getWindowPosition.Enable = function() {
 	this.flag = 1;
 };
 
-getWindowPosition.Disable = function() {	//拔旗，對不起這是作者的惡趣味
+getWindowPosition.Disable = function() {
 	this.flag = 0;
 };
 
